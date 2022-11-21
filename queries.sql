a983f401dde1cbba2d0babab87736ec3755ea5ba
lee rohrer <lrohrer@hunter.com>
2022-11-20 18:45:25 -0600

    Finished

--- queries.sql
+++ queries.sql
@@ -1,7 +1,17 @@
 -- Part 1: Test it with SQL
+--Id int (primary key), name VARCHAR(150), employer VARCHAR(150), skill VARCHAR(150)
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 -- Part 3: Test it with SQL
+DROP table job;
 
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
